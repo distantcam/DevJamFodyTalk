b5c536cfa495df42d502df8fd0264a6b28a5786b
Cameron MacFarland <distantcam@gmail.com>
2015-12-02 16:40:09 +0800

    Fullscreen slides

--- Slides/MainWindow.xaml.cs
+++ Slides/MainWindow.xaml.cs
@@ -19,6 +19,13 @@ namespace Slides
         {
             base.OnKeyUp(e);
 
+            if (e.Key == Key.Escape)
+            {
+                this.WindowState = WindowState.Normal;
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                return;
+            }
+
             if (e.Key == Key.Left)
                 currentState = Math.Max(currentState - 1, 0);
             else if (currentState == slides.States.Length - 1)
@@ -27,6 +34,14 @@ namespace Slides
                 currentState = Math.Min(currentState + 1, slides.States.Length - 1);
 
             VisualStateManager.GoToState(slides, slides.States[currentState], true);
+        }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
         }
     }
 }
