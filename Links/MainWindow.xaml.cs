37c146ed5cbfa2d197c71eb420a1bd464657404c
Cameron MacFarland <distantcam@gmail.com>
2015-12-03 12:33:39 +0800

    Added links slide at the end

---
+++ Links/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace Links
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            if (e.Key == Key.Escape)
+            {
+                this.WindowState = WindowState.Normal;
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                return;
+            }
+        }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
